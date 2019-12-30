58e8a42355457ce75538f3ceeeb41ae7c04d2d72
hatemets <mistermark009@gmail.com>
2019-12-30 16:19:17 +0200

    Create navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import M from 'materialize-css'
+
+const Navbar = () => {
+  useEffect(() => {
+    let elements = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elements)
+  }, [])
+
+  const navLinks = <div>
+    <li><a href="/#">Home</a></li>
+    <li><a href="/#">Profile</a></li>
+    <li><a href="/#">New Post</a></li>
+    </div>
+
+  return (
+    <header>
+      <nav className="navbar blue-grey">
+        <div className="container">
+          <a href="/#" className="brand-logo">Dog Food</a>
+          <a href="/#" className="sidenav-trigger" data-target="mobile-nav">
+            <i className="material-icons">menu</i>
+          </a>
+
+          <ul className="right hide-on-med-and-down">
+            {navLinks}
+          </ul>
+        </div>
+      </nav>
+
+      <ul className="sidenav" id="mobile-nav">
+        {navLinks}
+      </ul>
+    </header>
+  )
+}
+
+export default Navbar
